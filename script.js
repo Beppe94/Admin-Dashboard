d1b02e127ad595c71738fc869525955e60a092ef
Beppe94 <giuseppe.monorchio94@hotmail.it>
2022-10-29 20:06:30 +0200

    added collapsing sidebar

---
+++ script.js
@@ -0,0 +1,11 @@
+const toggle = document.getElementById('toggle')
+const sidebar = document.getElementById('sidebar')
+
+toggle.addEventListener('click', () => {
+    if(!sidebar.style.left || sidebar.style.left === '-250px') {
+        sidebar.style.left = '0'
+    } else {
+        sidebar.style.left = '-250px'
+    }
+})
+
